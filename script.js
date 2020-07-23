7dc957375bea58578eac8ae69886c4440102c813
TyTy <Tylieafter1@gmail.com>
2020-07-23 17:49:53 +0000

    nothing yet beta testing in another tab

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ bubblesX2();
 myFunction();
 }
 
-
 //function for bubbles
 function bubbles() {
 for (var i = 0; i < 200; i++) {
-  rgb (randomNumber(100,200),randomNumber(100,200),randomNumber(100,200) ,0.5);
+  rgb (randomNumber(100,255),randomNumber(100,255),randomNumber(100,255) ,0.5);
   moveTo(randomNumber(0,320), randomNumber(0,450));
-  dot(randomNumber(1,20));
+  dot(randomNumber(1,200));
   moveTo(randomNumber(0,320),randomNumber(0,450));
  }
 }
@@ -19,9 +18,9 @@ for (var i = 0; i < 200; i++) {
 // function for stable bubbles
 function bubblesX2() {
 for (var i = 0; i < 400; i++) {
-  rgb (randomNumber(100,200),randomNumber(100,200),randomNumber(100,200));
+  rgb (randomNumber(100,255),randomNumber(100,255),randomNumber(100,255));
   moveTo(randomNumber(0,320), randomNumber(0,450));
-  dot(randomNumber(1,10));
+  dot(randomNumber(1,100));
   moveTo(randomNumber(0,320),randomNumber(0,450));
  }
 }
@@ -29,25 +28,25 @@ for (var i = 0; i < 400; i++) {
 function myFunction() {
   var x = document.getElementById("div.image");
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "image";
   } else {
     x.style.display = "none";
   }
   var x = document.getElementById("div.imageTwo");
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "imageTwo";
   } else {
     x.style.display = "none";
   }
   var x = document.getElementById("div.imageThree");
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "imageThree";
   } else {
     x.style.display = "none";
   }
   var x = document.getElementById("div.imageFour");
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "imageFour";
   } else {
     x.style.display = "none";
   }
